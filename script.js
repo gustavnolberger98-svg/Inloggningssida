ce3cd5beb4a07d87417b22d64a8dba4daa87df2e
gustavnolberger98-svg <gustav.nolberger98@gmail.com>
2026-09-23 22:09:29 +0200

    update javascript

--- script.js
+++ script.js
@@ -6,18 +6,22 @@ const message = document.getElementById("message");
 
 const button = document.getElementById("logout");
 
-const vidare = document.getElementById("vidare")
+const vidare = document.getElementById("vidare");
+
+const form = document.getElementById("formen");
 
 vidare.addEventListener("click", e=>{login()
 })
 
-function login() {
+function login(e) {
 const username = document.getElementById("username").value;
 
 const password = document.getElementById("password").value;
 
 if(username === correctname && password === correctpassword){
+    form.display(none)
     document.getElementById("logout")
+    e.preventDefault()
 }
 else{
     message.textContent = "Fel Namn eller lösenord";
