c2daa470ba84fffbd11d82e1e928b2d0f3fe3429
gustavnolberger98-svg <gustav.nolberger98@gmail.com>
2026-09-23 16:10:50 +0200

    Ändrar lite namn och lägger till variablar
    Blev förvirrande med mina gamla namn

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const correctname = "Kalle";
+
 const correctpassword = "qwe123";
+
+const username = document.getElementById("username").value;
+
+const password = document.getElementById("password").value;
+
+const message = document.getElementById("message");
+
+const button = document.getElementById("button");
 
 
 function login() {
-const username = document.getElementById("namn").value;
-const password = document.getElementById("password").value;
-const message = document.getElementById("message");
 
-if(namn===correctname && password === correctpassword){
-    message.textContent = "Du är nu inloggad";
+button.addEventListener("click", e=>{
+    text.innerhtml = "du är inloggad";
+})
+
+if(username === correctname && password === correctpassword){
+    console.log(message)
 }
 else{
     message.textContent = "Fel Namn eller lösenord";
+    console.log("du är fan sämst")
 }
 }
