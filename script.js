983eb1c3dd52ccdd79ead77b0d03d8f8c56e4d55
gustavnolberger98-svg <gustav.nolberger98@gmail.com>
2026-09-24 10:22:43 +0200

    Logga ut-knappen
    Lägger till logout och gör den hidden. I funktionen tillagt så att man får bort formulär och visar logga ut-knappen. Placering behöver ändras

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ const correctpassword = "qwe123";
 const message = document.getElementById("message");
 
 const button = document.getElementById("logout");
+button.style.display = "none"
 
 const vidare = document.getElementById("vidare");
 
 const form = document.getElementById("formen");
 
-vidare.addEventListener("click", e=>{login()
+vidare.addEventListener("click", e=>{login(e)
 })
 
 function login(e) {
@@ -19,9 +20,9 @@ const username = document.getElementById("username").value;
 const password = document.getElementById("password").value;
 
 if(username === correctname && password === correctpassword){
-    form.display(none)
-    document.getElementById("logout")
-    e.preventDefault()
+    form.style.display = "none";
+    button.style.display = "block";
+    e.preventDefault();
 }
 else{
     message.textContent = "Fel Namn eller lösenord";
