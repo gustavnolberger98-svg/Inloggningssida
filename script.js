6f002e77209ce46c5c9423af43cee42d98ec1c3f
gustavnolberger98-svg <gustav.nolberger98@gmail.com>
2026-09-25 01:32:49 +0200

    Localstorage fix
    Kollade i genom videos på canvas och fattade tillslut

--- script.js
+++ script.js
@@ -13,8 +13,16 @@ const form = document.getElementById("formen");
 
 const welcome = document.getElementById("welcome");
 
+const inloggad = localStorage.getItem("inloggad");
+
 vidare.addEventListener("click", e=>{login(e)
 })
+
+if(inloggad === "true"){
+    form.style.display = "none";
+    button.style.display = "block";
+    welcome.style.display = "block";
+}
 
 function login(e) {
 const username = document.getElementById("username").value;
@@ -24,11 +32,10 @@ const password = document.getElementById("password").value;
 if(username === correctname && password === correctpassword){
     form.style.display = "none";
     button.style.display = "block";
-    welcome.style.display = "block"
+    welcome.style.display = "block";
     welcome.textContent = "Välkommen " + correctname;
     e.preventDefault();
-    localStorage.setItem("correctname", correctname);
-    localStorage.setItem("correctpassword", correctpassword);
+    localStorage.setItem("inloggad", "true");
 }
 else{
     message.style.display = "block";
@@ -42,5 +49,6 @@ button.addEventListener("click", logout);
 function logout() {
         form.style.display = "block";
         button.style.display = "none";
+        message.textContent = "";
         localStorage.clear();
     }
